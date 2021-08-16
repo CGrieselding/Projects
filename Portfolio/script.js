7267e63a8a6df2f7f6593e2b00bad2604333b43d
Cameron Grieselding <cameron@Owner-PC.localdomain>
2021-08-16 09:32:21 -0400

    update push on Aug16

---
+++ Portfolio/script.js
@@ -0,0 +1,11 @@
+let nav = document.querySelector('nav')
+let lastKnownScrollPosition
+
+document.addEventListener('scroll', function(e) {
+    lastKnownScrollPosition = window.scrollY
+    if (lastKnownScrollPosition !== 0) {
+        nav.setAttribute('class', 'navbar navbar-expand-lg bg-secondary bg-gradient navbar-dark')
+    } else if (lastKnownScrollPosition == 0) {
+        nav.setAttribute('class', 'navbar navbar-expand-lg bg-transparent navbar-dark' )
+    }
+});
